cbfd40dd52e13c9111df15bb1a12e6eefadfb746
wangd14 <wangd14@rpi.edu>
2024-12-20 01:52:22 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
